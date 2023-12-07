23aec5f9ae0e94ce5b93c67417b1078375ad3eaa
Martin King <martinkinghello@gmail.com>
2023-12-07 18:43:30 -0500

    Assignment 2

---
+++ assignment2/ejs/calculate.mjs
@@ -0,0 +1,13 @@
+const calculateArea = (length, width) => {
+  return length * width;
+};
+
+const calculateVolume = (length, width, height) => {
+  return length * width * height;
+};
+
+const calculateSquare = (length) => {
+  return length * length;
+};
+
+export { calculateArea, calculateVolume, calculateSquare };
